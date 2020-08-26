3d1c20b1af89d34c1cc7675d650c3c443e34f1b6
root <root@ticket.zoomy.lk>
2020-08-26 11:10:44 +0000

    26082020

---
+++ upload/include/plugins/plugin-fwd-rewriter-master/plugin.php
@@ -0,0 +1,9 @@
+<?php return array(
+ 'id' => 'clonemeagain:rewriter', # notrans
+ 'version' => '1.1',
+ 'name' => 'Ticket Rewriter',
+ 'author' => 'clonemeagain@gmail.com',
+ 'description' => 'Modifies an inbound ticket\'s details to be the original sender from a forwarded message.',
+ 'url' => 'https://github.com/clonemeagain/plugin-fwd-rewriter',
+ 'plugin' => 'class.RewriterPlugin.php:RewriterPlugin'
+);
